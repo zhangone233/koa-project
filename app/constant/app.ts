e09839f18e978595cb70dbf4ec6ff9e2d22661e9
zhangyi <zhangone233@126.com>
2023-08-28 16:09:13 +0800

    feat: 切换appid

--- app/constant/app.ts
+++ app/constant/app.ts
@@ -1,6 +1,10 @@
-/** 这个是小程序的 */
-export const APP_ID = 'tt21355788be0b6fd101';
-export const APP_SECRET = '9888a26bf2ffc05e57d7d395ecee913d08c779ed';
+/** 这个是demo小程序的 */
+// export const APP_ID = 'tt21355788be0b6fd101';
+// export const APP_SECRET = '9888a26bf2ffc05e57d7d395ecee913d08c779ed';
+
+/** 这个是泸州老窖小程序的 */
+export const APP_ID = 'tt30fc4641afc1dafa01';
+export const APP_SECRET = 'd409084dcaea2d4f436f52c9926e8130b948ed22';
 
 /** ctx.headers要验证的会话token名字 */
 export const HEADER_TOKEN_NAME = 'x-test-token';
